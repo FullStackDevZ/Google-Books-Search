2c7ee8ab68aec31e715964ab78bb85b0718c1033
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-07-20 23:04:04 -0700

    added models

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,17 +1,13 @@
 import React from "react";
-import './NavBar.css';
-import DisplayMessage from '../ClickyGameContent';
 
-const Navbar = (props) => {
-    return(
-        <div className='header'>
-            <ul>
-                <li><h2 className='title animated swing'>NB 90's Clicky Game</h2></li>
-                <DisplayMessage score={props.score} topScore={props.topScore} />
-                <li id='score'> Current Score: {props.score}  |  Your Top Score: {props.topScore}</li>
-            </ul>
-        </div>
-    )
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        React Reading List
+      </a>
+    </nav>
+  );
 }
 
-export default Navbar;
+export default Nav;
